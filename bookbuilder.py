0fd28a0968e8e238bd67cd51c4be7994a355e32b
Peter Savage <silentkeystroke@googlemail.com>
2012-02-26 15:11:29 +0000

    More work on bookbuilder script

--- bookbuilder.py
+++ bookbuilder.py
@@ -1,8 +1,32 @@
 import xml.etree.ElementTree as ET
+import os, os.path
+import shutil
+
+def processpart(part, path, count):
+	start_chap = int(part.find("chapters").attrib['start'])
+	end_chap = int(part.find("chapters").attrib['end'])
+
+def processbook(book):
+	book_name = book.attrib['name']
+	book_id = book.attrib['id']
+	path = "source/" + book_id
+	
+	print "\tProcessing: " + book_name
+	shutil.rmtree(path + "/tmp")
+	os.mkdir(path + "/tmp")
+	count = 1
+	parts = book.findall("part")
+	for part in parts:
+		processpart(part, path, count)
+		count += 1
 
 xml = ET.parse("schema.xml")
 
 books = xml.findall("book")
+
+formats = xml.find("parameters/formats")
+
+formats_required = []
 
 series_name = xml.getroot().attrib['name']
 print "Series: " + series_name
@@ -12,3 +36,12 @@ print "Books:"
 for book in books:
 	books_name.append(book.attrib['name'])
 	print "\t" + books_name[-1]
+
+print "Formats:"
+for forma in formats:
+	formats_required.append(forma.tag)
+	print "\t" + formats_required[-1]
+	
+print "Processing Books:"
+for book in books:
+	processbook(book)
